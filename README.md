4e544508a3171b2e0baba4ab315849d2d43f5bd8
Alfredo <alfredo_rdc@msn.com>
2025-08-04 17:40:09 -0600

    Se dejan solo checkbox para que el usuario seleccione que graficar

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # proyecto_sprint_7
 Proyecto del sprint 7 tripleten
+Se crea app.py para graficar histogramas y diagrama de dispersion.
--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-car_data = pd.read_csv('C:\Users\Alfredo Díaz J\Desktop\proyecto_final_sprin_7\proyecto_sprint_7\vehicles_us.csv')
-hist_button = st.button('Construir histograma')
-if hist_button:
-    st.write(
-        'Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+car_data = pd.read_csv(
+    r'C:\Users\Alfredo Díaz J\Desktop\proyecto_final_sprin_7\proyecto_sprint_7\vehicles_us.csv')
+
 build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram:
     st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+build_dispersion = st.checkbox('Construir un gráfico de dispersión')
+if build_dispersion:
+    st.write('Construir un gráfico de dispersión para las columnas odómetro y precio')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
