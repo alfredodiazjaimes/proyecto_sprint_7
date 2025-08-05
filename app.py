75b0fae79d09e96871606813953d80b2ae1599ee
Alfredo <alfredo_rdc@msn.com>
2025-08-05 02:34:13 -0600

    Se agrega codigo para mostrar el DataFrame original y una checkbox para un filtro en especifico

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ import pandas as pd
 import plotly.express as px
 car_data = pd.read_csv(
     r'C:\Users\Alfredo Díaz J\Desktop\proyecto_final_sprin_7\proyecto_sprint_7\vehicles_us.csv')
+st.header('Análisis de Datos de Vehículos')
+st.write('Visualización de datos de vehículos usados en Estados Unidos')
+
+filtrar_por_año = st.checkbox('Filtrar modelos del año 2015 en adelante')
+if filtrar_por_año:
+    filtro_año = car_data[car_data['model_year'] >= 2015]
+    st.write('Carros del año 2015 en adelante')
+    st.write(filtro_año)
+else:
+    st.write('Todos los carros')
+    st.write(car_data)
 
 build_histogram = st.checkbox('Construir un histograma')
 
